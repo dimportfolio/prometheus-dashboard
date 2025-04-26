2938c7a33023db98fa103361a55d85f3dcec1360
Daniel Im <samanco@Daniels-MacBook-Pro.local>
2025-04-26 19:58:50 -0400

    feat: connect frontend service layer to local Express backend

--- src/services/prometheusService.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-
-export const fetchCPUUsage = () => axios.get('/api/cpu').then(res => res.data);
-export const fetchMemoryUsage = () => axios.get('/api/memory').then(res => res.data);
