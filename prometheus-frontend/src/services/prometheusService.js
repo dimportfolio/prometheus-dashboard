ffc3f082c8cc4c3201ff75b4b555ca874015f2d9
Daniel Im <samanco@Daniels-MBP.localdomain>
2025-04-26 20:13:08 -0400

    feat: update service layer to parse Prometheus-style API responses

--- prometheus-frontend/src/services/prometheusService.js
+++ prometheus-frontend/src/services/prometheusService.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api'; // NEW backend address
+const BASE_URL = 'http://localhost:5000/api';
 
-export const fetchCPUUsage = () => 
-  axios.get(`${BASE_URL}/cpu`).then(res => res.data);
+export const fetchCPUUsage = async () => {
+  const res = await axios.get(`${BASE_URL}/cpu`);
+  const [timestamp, usage] = res.data.data.result[0].value;
+  return { timestamp, usage };
+};
 
-export const fetchMemoryUsage = () => 
-  axios.get(`${BASE_URL}/memory`).then(res => res.data);
+export const fetchMemoryUsage = async () => {
+  const res = await axios.get(`${BASE_URL}/memory`);
+  const [timestamp, used] = res.data.data.result[0].value;
+  return { timestamp, used };
+};
